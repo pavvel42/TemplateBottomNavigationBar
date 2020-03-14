0f892973b522adcb10e7eb168f44a30ad74f9a4a
pavvel42 <pavvel.durda@gmail.com>
2020-03-14 11:41:26 +0100

    zmiana fragmentu

---
+++ app/src/main/java/com/example/templatebottomnavmenu/ui/home/HomeFragment.kt
@@ -0,0 +1,59 @@
+package com.example.templatebottomnavmenu.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
+import com.example.templatebottomnavmenu.R
+import com.example.templatebottomnavmenu.ui.dashboard.DashboardFragment
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var importEditText: EditText
+    private lateinit var saveButton: Button
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer { textView.text = it })
+        initialize(root)
+        return root
+    }
+
+    fun initialize(root: View) {
+        importEditText = root.findViewById(R.id.importText)
+        saveButton = root.findViewById(R.id.buttonSave)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showToast(view)
+    }
+
+    fun showToast(view: View){
+        saveButton.setOnClickListener {
+            Toast.makeText(context,importEditText.text,Toast.LENGTH_SHORT).show()
+            changeFragment(view)
+        }
+    }
+
+    fun changeFragment(view: View){
+        val manager = parentFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment.toInt(), DashboardFragment())
+        transaction.commit()
+        val navController = findNavController(view)
+        navController.navigate(R.id.navigation_dashboard)
+    }
+}
