74f8169bb46a96e0de673b3e4a8a23321ef49fc3
pavvel42 <pavvel.durda@gmail.com>
2020-06-21 01:32:39 +0200

    firebase logout

--- app/src/main/java/com/example/templatebottomnavmenu/MainActivity.kt
+++ app/src/main/java/com/example/templatebottomnavmenu/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.templatebottomnavmenu
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialize()
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -22,5 +32,24 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun initialize(){
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = Firebase.auth //ktx
+        // [END initialize_auth]
+    }
+
+    private fun signOut(){
+        auth.signOut()
+        Log.d(TAG, "logout:success")
+        finish()
+        startActivity(Intent(this@MainActivity,GoogleSignInActivity::class.java))
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        signOut()
     }
 }
